ac3b02b314b0df9f00863b07580faae3170137db
Josh Nothum <joshnothum@gmail.com>
2017-10-10 18:49:45 -0500

    lack monthlyCost

--- server/public/scripts/client.js
+++ server/public/scripts/client.js
@@ -2,27 +2,26 @@ $(document).ready(readyNow);
 
  function readyNow() {
    console.log('js sourced');
+   clickHandler();
 
  }
  function clickHandler() {
 
-   $('#submit').on('click', appendInfo);
+   $('#button').on('click', appendInfo);
 
  }
 var monthlyCost = 0;
 function appendInfo() {
   event.preventDefault();
-  var $tr = $('#employee').append('<tr></tr>');
-  $tr.append('<td>'+$('#firstName').val()+'</td>');
-  $tr.append('<td>'+$('#lastName').val()+'</td>');
-  $tr.append('<td>'+$('#title').val()+'</td>');
-  $tr.append('<td>'+$('#employeeID').val()+'</td>');
-  $tr.append('<td>'+$('#annualSalary').val()+'</td>');
-
-  monthlyCost();
+$('#container').append("<p>Hello</p>");
+var $tr = $('<')
+console.log('help');
+monthlyCost();
 
 }
 
  function monthlyCost(){
-   monthlyCost = annualSalary/12;
+   var annualSalary = parseInt($('#annualSalary').val()));
+   monthlyCost += annualSalary/12;
+   return monthlyCost;
  }
