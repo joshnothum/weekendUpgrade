d3dbfc803605a69ee91c04c13d21fee9d09e7cd3
Josh Nothum <joshnothum@gmail.com>
2017-10-10 20:17:16 -0500

    monthly average working

--- server/public/scripts/client.js
+++ server/public/scripts/client.js
@@ -11,17 +11,25 @@ $(document).ready(readyNow);
 
  }
 var monthlyCost = 0;
+
 function appendInfo() {
-  event.preventDefault();
-$('#container').append("<p>Hello</p>");
-var $tr = $('<')
+event.preventDefault();
+averageMaker();
+console.log(monthlyCost);
+
+var $newInfo = $("<p class = 'info'></p>");
+$('#monthlyCost').text(monthlyCost);
+
 console.log('help');
-monthlyCost();
+
+
+
 
 }
 
- function monthlyCost(){
-   var annualSalary = parseInt($('#annualSalary').val()));
-   monthlyCost += annualSalary/12;
+ function averageMaker(){
+   
+   monthlyCost += parseInt(($('#annualSalary').val())/12);
    return monthlyCost;
+
  }
